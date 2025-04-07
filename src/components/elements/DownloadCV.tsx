62108cf4fa3ddc3997a8e602354f363a0df81a78
rayhanfuadi <oke.rayhan1@gmail.com>
2025-04-07 21:31:31 +0700

    updated cv

--- src/components/elements/DownloadCV.tsx
+++ src/components/elements/DownloadCV.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const DownloadCV = () => {
     return (
         <>
-            <a href="/CV_MuhammadRayhanFuadi.pdf"
+            <a href="/CV ATS Muhammad Rayhan Fuadi.pdf"
                 download
                 className="flex items-center gap-3 py-4 px-8 w-fit border border-[#158048] hover:bg-[#158048] text-white rounded-full xl:text-xl lg:text-lg text-sm uppercase font-normal transition-all duration-300 ease-in-out"
             >
