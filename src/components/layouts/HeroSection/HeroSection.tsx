ce4239f6eb55877a3d629aa28d0d96452d4d176d
rayhanfuadi <oke.rayhan1@gmail.com>
2025-07-03 02:50:22 +0700

    updated Running Text

--- src/components/layouts/HeroSection/HeroSection.tsx
+++ src/components/layouts/HeroSection/HeroSection.tsx
@@ -3,12 +3,20 @@ import Image from "next/image"
 import Navbar from "../Navbar"
 import Located from "@/components/fragments/Located"
 import BackgroundField from "@/components/fragments/BackgroundField"
-import RunningText from "@/components/elements/RunningText"
+// import RunningText from "@/components/elements/RunningText"
+import ScrollVelocity from "@/blocks/TextAnimations/ScrollVelocity/ScrollVelocity"
 
 const HeroSection = () => {
     return (
         <div className="relative">
-            <RunningText />
+            {/* <RunningText /> */}
+            <div className="absolute w-full xl:h-[330px] lg:h-[234px] h-[120px] left-0 bottom-0 z-50 bg-gradient-to-t from-[#000C04] to-transparent overflow-hidden uppercase font-normal">
+                <ScrollVelocity
+                    texts={['Front End Developer 🚀 UI/UX Designer 🚀', '']}
+                    velocity={150}
+                    className="custom-scroll-text"
+                />
+            </div>
             <div className="relative xl:pt-[98px] lg:pt-[70px] pt-[36px]">
                 <div className="absolute w-full left-0 top-0">
                     <Navbar />
