45da521811904ed7f634731fe44f19dac82b12af
rizkyamanda12399-lab <rizkyamanda12399@gmail.com>
2025-08-17 03:15:08 +0700

    test

--- src/components/elements/ButtonCustom.tsx
+++ src/components/elements/ButtonCustom.tsx
@@ -1,5 +1,5 @@
 import { cva, type VariantProps } from 'class-variance-authority'
-
+// test
 const buttonVariants = cva("flex items-center gap-2 text-sm font-medium rounded-md w-fit", {
     variants: {
         background: {
